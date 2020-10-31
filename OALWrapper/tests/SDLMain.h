3dfc45e5fc4c031b2844c7570a17091f64ab9a70
zenmumbler <zen@stardazed.club>
2020-10-31 23:13:13 +0100

    chore: OALWrapper project

---
+++ OALWrapper/tests/SDLMain.h
@@ -0,0 +1,11 @@
+/*   SDLMain.m - main entry point for our Cocoa-ized SDL app
+t   Initial Version: Darrell Walisser <dwaliss1@purdue.edu>
+t   Non-NIB-Code & other changes: Max Horn <max@quendi.de>
+
+tFeel free to customize this file to suit your needs
+*/
+
+#import <Cocoa/Cocoa.h>
+
+@interface SDLMain : NSObject
+@end
