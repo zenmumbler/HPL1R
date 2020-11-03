85156cbf9da0445009717e192a467d31cec87741
zenmumbler <zen@stardazed.club>
2020-11-03 23:26:34 +0100

    feat(graphics): implement basic 32-bit BMP loader

---
+++ HPL/include/impl/SDLBMPFile.h
@@ -0,0 +1,22 @@
+//
+//  SDLBMPFile.h
+//  HPL1R - BMP file reader because SDL2_image does not support 32-bit BMPs
+//
+//  Created by zenmumbler on 03/11/2020.
+//
+
+#ifndef HPL_SDLBMPFILE_H
+#define HPL_SDLBMPFILE_H
+
+#include <SDL2/SDL.h>
+
+#include "system/SystemTypes.h"
+
+namespace hpl {
+
+	SDL_Surface* LoadBMPFileUsingSDL(const tString& asFilePath);
+	SDL_Surface* LoadBMPFile(const tString& asFilePath);
+
+}
+
+#endif /* HPL_SDLBMPFILE_H */
