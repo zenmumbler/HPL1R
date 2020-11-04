0374e2371c19f125155ad0fffb6a36546c3ebf38
zenmumbler <zen@stardazed.club>
2020-11-04 21:20:19 +0100

    feat(system): custom 32-bit TGA loader
    SDL2 on macOS premultiplies the alpha, rendering the image useless for in HPL

---
+++ HPL/include/impl/SDLTGAFile.h
@@ -0,0 +1,22 @@
+//
+//  SDLTGAFile.h
+//  HPL1R - TGA image load wrapper around SDL2_image
+//
+//  Created by zenmumbler on 04/11/2020.
+//
+
+#ifndef HPL_SDLTGAFILE_H
+#define HPL_SDLTGAFILE_H
+
+#include <SDL2/SDL.h>
+
+#include "system/SystemTypes.h"
+
+namespace hpl {
+
+	SDL_Surface* LoadTGAFileUsingSDLImage(const tString& asFilePath);
+	SDL_Surface* LoadTGAFile(const tString& asFilePath);
+
+}
+
+#endif /* HPL_SDLTGAFILE_H */
