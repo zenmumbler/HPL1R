56be0f3198cf2b83e5b86170215da46c6694a979
zenmumbler <zen@stardazed.club>
2020-11-19 22:28:09 +0100

    chore: remove old tests

--- HPL/tests/PhysicsTest/PhysicsTest.h
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2007-2010 (C) - Frictional Games
- *
- * This file is part of HPL1Engine
- *
- * For conditions of distribution and use, see copyright notice in LICENSE-tests
- */
-#pragma once
