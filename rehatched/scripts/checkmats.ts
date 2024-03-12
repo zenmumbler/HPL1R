60fb6bed9fe17020b12098fe49c6b2e0f4f740fb
zenmumbler <zen@stardazed.club>
2024-03-12 14:14:29 +0100

    chore: test script stuff

--- rehatched/scripts/checkmats.ts
+++ rehatched/scripts/checkmats.ts
@@ -2,46 +2,49 @@ import { readdirSync, readFileSync } from "fs";
 import { extname } from "path";
 import { resolve, join } from "path/posix";
 
-//const path = resolve("../../Build/Debug");
-const path = "/Applications/Games/Penumbra Black Plague.app/Contents/Resources/game";
+const path = resolve("../../Build/Debug");
+// const path = "/Applications/Games/Penumbra Black Plague.app/Contents/Resources/game";
 
 let matCount = 0;
 const matFiles: Record<string, string[]> = {};
 const dep: Record<string, string[]> = {};
+const fileTypes: Record<string, number> = {};
+const listing: Record<string, { n: string, kb: number }[]> = {};
+
+function doFunnyStuffWithFile(fileName: string, fullPath: string) {
+	matCount++;
+	const text = readFileSync(fullPath, { encoding: "utf-8" });
+	const match = text.match(/Wrap="(\w*)"/);
+	if (match) {
+		const renderType = match[1].toLowerCase();
+		if (! (renderType in matFiles)) {
+			matFiles[renderType] = [];
+		}
+		matFiles[renderType].push(fileName);
+	}
+	else {
+		console.error("  Can't match renderType in " + fileName);
+	}
+}
 
 function scan(dir: string) {
 	const files = readdirSync(dir, { withFileTypes: true });
 	for (const f of files) {
+		const fullPath = join(dir, f.name);
 		if (f.isDirectory()) {
 			// console.info("DIR", f.name);
 			scan(join(dir, f.name));
 		}
 		else if(f.isFile()) {
+			matCount++;
 			const ext = extname(f.name).toLowerCase();
+			if (ext.length == 0) continue;
+			// if (! (ext in fileTypes)) { fileTypes[ext] = 0; listing[ext] = []; }
+			// fileTypes[ext]++;
+			// listing[ext].push({ n: f.name, kb: Bun.file(fullPath).size });
+			
 			if (ext === ".mat") {
-				matCount++;
-				// console.info("  F", f.name);
-				const text = readFileSync(join(dir, f.name), { encoding: "utf-8" });
-				const match = text.match(/<Main\s+Type="(\w+)"/);
-				if (match) {
-					const renderType = match[1].toLowerCase();
-					if (! (renderType in matFiles)) {
-						matFiles[renderType] = [];
-					}
-					matFiles[renderType].push(f.name);
-				}
-				else {
-					console.error("  Can't match renderType");
-				}
-				
-				const depMatch = text.match(/Wrap=\"(\w+)\"/);
-				if (depMatch) {
-					const depType = depMatch[1].toLowerCase();
-					if (! (depType in dep)) {
-						dep[depType] = [];
-					}
-					dep[depType].push(f.name);
-				}
+				doFunnyStuffWithFile(f.name, fullPath);
 			}
 		}
 	}
@@ -50,10 +53,18 @@ function scan(dir: string) {
 scan(path);
 
 console.info("====================================================================");
-console.info("Mat files: ", matCount);
-//console.info(JSON.stringify(matFiles, undefined, 4));
-console.info(JSON.stringify(dep, undefined, 4));
+console.info("Files: ", matCount);
+// console.info(JSON.stringify(fileTypes, undefined, 4));
+console.info("====================================================================");
+// console.info("Listing");
+// for (const k in listing) { listing[k].sort((a,b) => a.kb-b.kb); }
+// console.info(JSON.stringify(listing, undefined, 4));
+// console.info(JSON.stringify(dep, undefined, 4));
 // console.info(dep.true.length + dep.false.length)
 // console.info(Object.values(matFiles).reduce((a, b) => a + b.length, 0));
 // console.info(Object.keys(matFiles).sort());
-// console.info(JSON.stringify(matFiles.bumpspecular, undefined, 4));
+console.info(matFiles["clamptoedge"]);
+for (const m in matFiles) {
+	matFiles[m] = matFiles[m].length + "";
+}
+console.info(matFiles);
